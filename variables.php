1692a07b2e26899a99c06d61ac2f65a9279f2589
Alex Saveliev <lyolik@codeminders.com>
2015-11-13 14:43:02 +0200

    local variable in the function inside of namespace

--- variables.php
+++ variables.php
@@ -5,6 +5,7 @@ $global = 0;
 function f() {
 
 	global $global; // global var
+	$global++;
 	$local = 0;  // local var, unique
 	for ($i = 0; $i < 5; $i++) { 
 		// local var
@@ -17,4 +18,11 @@ function f() {
 
 function g() {
 	$local = 0; // local var, unique
+}
+
+namespace foo\bar\baz {
+	function h() {
+		$local = 0; // testing def path in the namespace
+		$local++;
+	}
 }
